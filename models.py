dac445c1740c0a289c88c3b6db085f2c020f9772
deVerick06 <elianeheri261206@gmail.com>
2025-11-10 17:53:59 -0300

    Criando o modelo de tabela de usuario, usando a hash ao inves da senha pura

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy import Column, Integer, String, Text, DateTime, Boolean, UniqueConstraint
 from sqlalchemy.sql import func
 from database import Base
 
@@ -16,4 +16,13 @@ class MemoriaModel(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String(200), nullable=False)
     description = Column(Text, nullable=True)
+    criado_em = Column(DateTime(timezone=True), server_default=func.now())
+
+class UsuarioModel(Base):
+    __tablename__ = "usuarios"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String(100), nullable=False)
+    email = Column(String(255), unique=True, index=True, nullable=False)
+    hashed_password = Column(String(255), nullable=False)
     criado_em = Column(DateTime(timezone=True), server_default=func.now())
