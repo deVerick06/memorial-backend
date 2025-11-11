affbd3f0bb5648d8d30251f946d2035aaf507da6
deVerick06 <elianeheri261206@gmail.com>
2025-11-10 23:39:07 -0300

    Criando a rota para acesso com Token

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from database import engine, Base
 import models, schemas, database
 from sqlalchemy.orm import Session
 import security
+from fastapi.security import OAuth2PasswordRequestForm
 
 app = FastAPI()
 
@@ -76,3 +77,23 @@ def create_user(usuario: schemas.UsuarioCreate, db: Session = Depends(database.g
     db.refresh(new_user)
 
     return new_user
+
+@app.post("/token", response_model=schemas.Token)
+def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(database.get_db)
+):
+    usuario = db.query(models.UsuarioModel).filter(
+        models.UsuarioModel.email == form_data.username
+    ).first()
+
+    if not usuario or not security.verify_password(form_data.password, usuario.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Email ou senha incorretos",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token = security.create_access_token(
+        subject=usuario.email
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
