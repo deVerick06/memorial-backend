48f584eb7a4c91f775abce45f90f0b52cebbabc2
deVerick06 <elianeheri261206@gmail.com>
2025-11-11 11:33:38 -0300

    Aumentando a seguranca, criei um leitor de token para verificar se o usuario da requisicao esta com token ativo

--- security.py
+++ security.py
@@ -3,8 +3,14 @@ import argon2
 from datetime import datetime, timedelta
 from jose import jwt
 from config import settings
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+import database, models, schemas
 
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     """Verifica se a senha pura corresponde ao hash salvo."""
@@ -34,3 +40,40 @@ def create_access_token(subject: str) -> str:
     )
 
     return encoded_jwt
+
+def get_current_user(
+        token: str = Depends(oauth2_scheme),
+        db: Session = Depends(database.get_db)
+) -> models.UsuarioModel:
+    """
+    Pega o token do header.
+    Decodifica o token.
+    Pega o email (o 'sub') de dentro dele.
+    Busca o usuario no banco de dados com esse email.
+    Retorna o objeto do usuario ou um erro.
+    """
+
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Não foi possível validar as credenciais",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(
+            token,
+            settings.SECRET_KEY,
+            algorithms=[settings.ALGORITHM]
+        )
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    
+    except jwt.PyJWTError :
+        raise credentials_exception
+    
+    user = db.query(models.UsuarioModel).filter(models.UsuarioModel.email == email).first()
+    if user is None:
+        raise credentials_exception
+    
+    return user
