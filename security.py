5aecf6fcb6ff9e7905c4c944ba060d6d641753c0
deVerick06 <elianeheri261206@gmail.com>
2025-11-10 23:06:39 -0300

    Criando func para acesso com o token

--- security.py
+++ security.py
@@ -1,5 +1,8 @@
 from passlib.context import CryptContext
 import argon2
+from datetime import datetime, timedelta
+from jose import jwt
+from config import settings
 
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
@@ -10,3 +13,24 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 def hash_password(password: str) -> str:
     """Gera um hash a partir de uma senha pura."""
     return pwd_context.hash(password)
+
+def create_access_token(subject: str) -> str:
+    """
+    Cria um nova Token do Acesso (nosso "cracha") para um usuario.
+    O 'subject' (assunto) do token sera o email do usuario.
+    """
+
+    expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode = {
+        "sub": subject,
+        "exp": expire
+    }
+
+    encoded_jwt = jwt.encode(
+        to_encode,
+        settings.SECRET_KEY,
+        algorithm=settings.ALGORITHM
+    )
+
+    return encoded_jwt
