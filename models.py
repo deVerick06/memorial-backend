a22eae41236260b4fbe2edd0c0e99476f9ad2282
deVerick06 <elianeheri261206@gmail.com>
2025-11-17 18:17:04 -0300

    Alterando a ordem pois estava dando conflito

--- models.py
+++ models.py
@@ -3,6 +3,28 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from database import Base
 
+class UsuarioModel(Base):
+    __tablename__ = "usuarios"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String(100), nullable=False)
+    email = Column(String(255), unique=True, index=True, nullable=False)
+    hashed_password = Column(String(255), nullable=False)
+    criado_em = Column(DateTime(timezone=True), server_default=func.now())
+    memorias = relationship("MemoriaModel", back_populates="owner")
+    homenagens = relationship("HomenagemModel", back_populates="owner")
+
+class MemoriaModel(Base):
+    __tablename__ = "memorias"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(200), nullable=False)
+    description = Column(Text, nullable=True)
+    criado_em = Column(DateTime(timezone=True), server_default=func.now())
+    image_url = Column(String(512), nullable=True)
+    owner_id = Column(Integer, ForeignKey("usuarios.id"), nullable=False)
+    owner = relationship("UsuarioModel", back_populates="memorias")
+
 class HomenagemModel(Base):
     __tablename__ = "homenagens"
 
@@ -16,31 +38,10 @@ class HomenagemModel(Base):
     owner_id = Column(Integer, ForeignKey("usuarios.id"), nullable=False)
     owner = relationship("UsuarioModel", back_populates="homenagens")
 
-class MemoriaModel(Base):
-    __tablename__ = "memorias"
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(200), nullable=False)
-    description = Column(Text, nullable=True)
-    criado_em = Column(DateTime(timezone=True), server_default=func.now())
-    image_url = Column(String(512), nullable=True)
-    owner_id = Column(Integer, ForeignKey("usuarios.id"), nullable=False)
-    owner = relationship("UsuarioModel", back_populates="memorias")
-
-class UsuarioModel(Base):
-    __tablename__ = "usuarios"
-
-    id = Column(Integer, primary_key=True, index=True)
-    nome = Column(String(100), nullable=False)
-    email = Column(String(255), unique=True, index=True, nullable=False)
-    hashed_password = Column(String(255), nullable=False)
-    criado_em = Column(DateTime(timezone=True), server_default=func.now())
-    memorias = relationship("MemoriaModel", back_populates="owner")
-    homenagens = relationship("HomenagemModel", back_populates="owner")
 
 class VelaModel(Base):
     __tablename__ = "velas"
-
+    
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("usuarios.id"), nullable=False)
     homenagem_id = Column(Integer, ForeignKey("homenagens.id"), nullable=False)
